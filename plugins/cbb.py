e8557bf68c50b6ba6951dbbfb29934eddc45fcb7
D4RK ‼️ <116873243+atiiif@users.noreply.github.com>
2024-04-10 18:06:26 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,11 +1,3 @@
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
-
-
-
 
 from pyrogram import __version__
 from bot import Bot
@@ -19,10 +11,11 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>OUR PUBLIC BOT :</b> <a href='https://t.me/publicfilesharerobot'> file share bot </a>  \n<b> 👨‍💻 Devloper:</b> <a href='https://t.me/ifeelscam'>SHAIKH ALI</a> \n<b> 🤖 Creator :</b> <a href='t.me/infoxe'> 𝙃𝙖𝙢𝙯𝙖 </a>",
+            text = f"<b>👨‍💻 Devloper:</b> <a href='https://t.me/ifeelscam'>SHAIKH ALI</a> \n<b> 🤖 Creator :</b> <a href='t.me/infoxe'> 𝙃𝙖𝙢𝙯𝙖 </a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
-                [ [ InlineKeyboardButton(" Source code ", url="https://t.me/+NeqCUg-QDxo2Nzll") ],
+                [ [ InlineKeyboardButton(" Source code ", url="https://t.me/+NeqCUg-QDxo2Nzll"),
+                  InlineKeyboardButton("Devloper" , url= "tg://user?id=6076683960")],
                     [
                         InlineKeyboardButton("🔒 Close", callback_data = "close")
                     ]
@@ -40,8 +33,3 @@ async def cb_handler(client: Bot, query: CallbackQuery):
 
 
 
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
